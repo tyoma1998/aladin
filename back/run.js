4d692de7463e99b320273f57fe4ebbe0be95b026
Artemiy Levitskiy <alevitskiy@jaxel.com>
2022-03-06 23:23:36 +0300

    sheet

--- back/run.js
+++ back/run.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
 // app.use('/test', require('./routes/test.routes'));
 // app.use('/album', require('./routes/album.routes'));
